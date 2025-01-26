366cfcc75bb8b7e1e28b7c987e2e3fe80f179c62
kareem daka <hbk.dood@gmail.com>
2025-01-26 21:56:19 +0200

    small update

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,13 +1,16 @@
-fx_version 'bodacious'
-games { 'rdr3', 'gta5' }
-version '2.1.0'
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+author 'krrm1'
+description 'Allows players to select a spawn point from a list of available locations'
+version '2.2.0'
 
 client_script { 
    'client.lua',
    'Config.lua'
 } 
 
-ui_page('interface/interface.html')
+ui_page 'interface/interface.html'
 
 files {
    'interface/interface.html',
